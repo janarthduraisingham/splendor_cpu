5a2bf793250013950a09f8f30a0bda8d4291fda4
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-11 23:06:00 +0100

    removed setup toggle

--- pages/two_player.py
+++ pages/two_player.py
@@ -20,24 +20,23 @@ initial_level_1_2 = ""
 initial_level_1_3 = ""
 initial_level_1_4 = ""
 
-if setup_toggle:
     
-    st.write("Please enter cards in colour-cost-points format:\n\n bla/blu/gre/red/whi for colour\n\n a 6 digit number for the cost for the respective colours\n\n a 1 digit number for the points\n\n e.g. blu333001")
+st.write("Please enter cards in colour-cost-points format:\n\n bla/blu/gre/red/whi for colour\n\n a 6 digit number for the cost for the respective colours\n\n a 1 digit number for the points\n\n e.g. blu333001")
     
-    cols = st.columns(4)
+cols = st.columns(4)
     
-    with cols[0]:
-        initial_level_1_1 = st.text_input("Level 1 Column 1 card")
-        initial_level_2_1 = st.text_input("Level 2 Column 1 card")
+with cols[0]:
+    initial_level_1_1 = st.text_input("Level 1 Column 1 card")
+    initial_level_2_1 = st.text_input("Level 2 Column 1 card")
         
-    with cols[1]:
-        initial_level_1_2 = st.text_input("Level 1 Column 2 card")
+with cols[1]:
+    initial_level_1_2 = st.text_input("Level 1 Column 2 card")
         
-    with cols[2]:
-        initial_level_1_3 = st.text_input("Level 1 Column 3 card")
+with cols[2]:
+    initial_level_1_3 = st.text_input("Level 1 Column 3 card")
         
-    with cols[3]:
-        initial_level_1_4 = st.text_input("Level 1 Column 4 card")
+with cols[3]:
+    initial_level_1_4 = st.text_input("Level 1 Column 4 card")
     
     
     
