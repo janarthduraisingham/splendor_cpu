e35fabb6a5484de7c34dd5bf6f0b1b6f868b9158
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-11 22:03:31 +0100

    added 3/4 player new game buttons

--- homepage.py
+++ homepage.py
@@ -16,9 +16,14 @@ st.write ("M. Andre (2014). Splendor [Board Game]. Space Cowboys")
 
 st.write("Choose game:")
 
-if st.button("New 2 player game"):
-    
+if st.button("New 2 player game"):    
     st.navigation("pages/two_player")
+    
+if st.button("New 3 player game"):
+    st.navigation("pages/three_player")
+    
+if st.button("New 4 player game"):
+    st.navigation("pages/four_player")
 
 
 two_player = st.Page("pages/two_player.py", title="2 Player Game", icon=":material/diamond:")
