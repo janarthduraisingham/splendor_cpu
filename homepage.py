912cfb170ac12f592d8344e59c8e2a3222c22849
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-11 21:36:06 +0100

    added homepage and 2 player game

---
+++ homepage.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 11:51:36 2024
+
+@author: jd_se
+"""
+import streamlit as st
+
+
+st.set_page_config(page_title="Splendor CPU", page_icon=":gem:",
+                   initial_sidebar_state='collapsed')
+
+st.title("Splendor: CPU system")
+
+st.write ("**CREDIT**")
+
+st.write("Choose game:")
+
+if st.button("New 2 player game"):
+    
+    st.navigate("pages/two_player")
+
+
---
+++ pages/two_player.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 11 21:30:30 2024
+
+@author: jd_se
+"""
+import streamlit as st
+
+st.header("2 Player Game")
+
+level_one = st.columns(4)
+level_two= st.columns(4)
+level_three = st.columns(4)
