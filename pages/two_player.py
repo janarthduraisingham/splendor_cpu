5a9caa172f4091d0d909132b12b3c9293c5eb434
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-12 13:45:33 +0100

    tried adding session_state to setup section

--- pages/two_player.py
+++ pages/two_player.py
@@ -9,60 +9,77 @@ import copy
 
 st.header("2 Player Game")
 
-card_1_1 = "0"
-card_1_2 = "0"
-card_1_3 = "0"
-card_1_4 = "0"
-card_2_1 = "0"
-card_2_2 = "0"
-card_2_3 = "0"
-card_2_4 = "0"
-card_3_1 = "0"
-card_3_2 = "0"
-card_3_3 = "0"
-card_3_4 = "0"
+# initialise 3x4 card tableau
+card_list = ['card_1_1',
+             'card_1_2',
+             'card_1_3',
+             'card_1_4',
+             'card_2_1',
+             'card_2_2',
+             'card_2_3',
+             'card_2_4',
+             'card_3_1',
+             'card_3_2',
+             'card_3_4',             
+             ]
+for card in card_list:
+    if card not in st.session_state:
+            st.session_state[card] = '0'
+#card_1_1 = "0"
+#card_1_2 = "0"
+#card_1_3 = "0"
+#card_1_4 = "0"
+#card_2_1 = "0"
+#card_2_2 = "0"
+#card_2_3 = "0"
+#card_2_4 = "0"
+#card_3_1 = "0"
+#card_3_2 = "0"
+#card_3_3 = "0"
+#card_3_4 = "0"
 
 cards_dict ={"0":"cards/test.jpg",
              "1":"cards/splendor.jpg"}
 
+st.write("Open the toggle to set up initial card tableu. Close when done")
+if st.toggle("Game setup"): 
+    st.write("Please enter cards in colour-cost-points format:\n\n bla/blu/gre/red/whi for colour\n\n a 5 digit number for the cost for the respective colours\n\n a 1 digit number for the points\n\n e.g. blu333001")
     
-st.write("Please enter cards in colour-cost-points format:\n\n bla/blu/gre/red/whi for colour\n\n a 5 digit number for the cost for the respective colours\n\n a 1 digit number for the points\n\n e.g. blu333001")
-    
-cols = st.columns(4)
-    
-with cols[0]:
-    initial_card_1_1 = st.text_input("Level 1 Column 1 card")
-    initial_card_2_1 = st.text_input("Level 2 Column 1 card")
-    initial_card_3_1 = st.text_input("Level 3 Column 1 card")
+    cols = st.columns(4)
         
-with cols[1]:
-    initial_card_1_2 = st.text_input("Level 1 Column 2 card")
-    initial_card_2_2 = st.text_input("Level 2 Column 2 card")
-    initial_card_3_2 = st.text_input("Level 3 Column 2 card")
+    with cols[0]:
+        st.session_state['card_1_1'] = st.text_input("Level 1 Column 1 card")
+        st.session_state['card_2_1'] = st.text_input("Level 2 Column 1 card")
+        initial_card_3_1 = st.text_input("Level 3 Column 1 card")
         
-with cols[2]:
-    initial_card_1_3 = st.text_input("Level 1 Column 3 card")
-    initial_card_2_3 = st.text_input("Level 2 Column 3 card")
-    initial_card_3_3 = st.text_input("Level 3 Column 3 card")
+    with cols[1]:
+        st.session_state['card_1_2'] = st.text_input("Level 1 Column 2 card")
+        initial_card_2_2 = st.text_input("Level 2 Column 2 card")
+        initial_card_3_2 = st.text_input("Level 3 Column 2 card")
         
-with cols[3]:
-    initial_card_1_4 = st.text_input("Level 1 Column 4 card")
-    initial_card_2_4 = st.text_input("Level 2 Column 4 card")
-    initial_card_3_4 = st.text_input("Level 3 Column 4 card")
+    with cols[2]:
+        initial_card_1_3 = st.text_input("Level 1 Column 3 card")
+        initial_card_2_3 = st.text_input("Level 2 Column 3 card")
+        initial_card_3_3 = st.text_input("Level 3 Column 3 card")
+            
+    with cols[3]:
+        initial_card_1_4 = st.text_input("Level 1 Column 4 card")
+        initial_card_2_4 = st.text_input("Level 2 Column 4 card")
+        initial_card_3_4 = st.text_input("Level 3 Column 4 card")
     
-if st.button("Confirm"):
-    card_1_1 = copy.deepcopy(initial_card_1_1)
-    card_1_2 = copy.deepcopy(initial_card_1_2)
-    card_1_3 = copy.deepcopy(initial_card_1_3)
-    card_1_4 = copy.deepcopy(initial_card_1_4)
-    card_2_1 = copy.deepcopy(initial_card_2_1)
-    card_2_2 = copy.deepcopy(initial_card_2_2)
-    card_2_3 = copy.deepcopy(initial_card_2_3)
-    card_2_4 = copy.deepcopy(initial_card_2_4)
-    card_3_1 = copy.deepcopy(initial_card_3_1)
-    card_3_2 = copy.deepcopy(initial_card_3_2)
-    card_3_3 = copy.deepcopy(initial_card_3_3)
-    card_3_4 = copy.deepcopy(initial_card_3_4)
+#if st.button("Confirm"):
+#    card_1_1 = copy.deepcopy(initial_card_1_1)
+#    card_1_2 = copy.deepcopy(initial_card_1_2)
+#    card_1_3 = copy.deepcopy(initial_card_1_3)
+#    card_1_4 = copy.deepcopy(initial_card_1_4)
+#    card_2_1 = copy.deepcopy(initial_card_2_1)
+#    card_2_2 = copy.deepcopy(initial_card_2_2)
+#    card_2_3 = copy.deepcopy(initial_card_2_3)
+#    card_2_4 = copy.deepcopy(initial_card_2_4)
+#    card_3_1 = copy.deepcopy(initial_card_3_1)
+#    card_3_2 = copy.deepcopy(initial_card_3_2)
+#    card_3_3 = copy.deepcopy(initial_card_3_3)
+#   card_3_4 = copy.deepcopy(initial_card_3_4)
 
 st.subheader("Game Board")
     
@@ -71,7 +88,10 @@ level_two= st.columns(4)
 level_three = st.columns(4)
 
 with level_one[0]:
-    st.image(cards_dict[card_1_1])
+    st.image(cards_dict[st.session_state['card_1_1'])
+    
+with level_one[1]:
+    st.image(cards_dict[st.session_state['card_1_2'])
     
 if st.button("change pic to splendor"):
     card_1_1 = "1"
