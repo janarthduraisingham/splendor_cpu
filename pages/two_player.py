efefe030f368576f277fd9325441d2fe6be7d261
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-12 14:50:14 +0100

    fixed bug in auto closing set up toggle after clicking confirm

--- pages/two_player.py
+++ pages/two_player.py
@@ -84,7 +84,7 @@ if st.toggle("Game setup", key='setup_toggle') and st.session_state['setup_compl
         
     if st.button("Confirm setup"):
         st.session_state['setup_complete'] = '1'
-        not st.session_state['setup_toggle']
+        st.session_state['setup_toggle'] = False
     
 #if st.button("Confirm"):
 #    card_1_1 = copy.deepcopy(initial_card_1_1)
