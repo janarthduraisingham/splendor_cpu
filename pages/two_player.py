d197d765bc1699252ec5796d7f0f3af57aeb5b0f
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-13 21:21:35 +0100

    removed unnecessary vars

--- pages/two_player.py
+++ pages/two_player.py
@@ -32,10 +32,6 @@ var_list = ['card_1_1',
              'tableau_deck',
              'p1_deck',
              'cpu_deck',
-             'post_setup_deck',
-             'lista',
-             'listb',
-             'listc',
              'intialise'
              ]
 
