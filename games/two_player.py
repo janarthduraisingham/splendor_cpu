d8aff64915e5ab18dc13381100090033a1b344f2
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-08-14 12:29:24 +0100

    moved all vars to session state

--- games/two_player.py
+++ games/two_player.py
@@ -6,12 +6,102 @@ Created on Sun Aug 11 21:30:30 2024
 """
 import streamlit as st
 from PIL import Image, ImageOps
-#from initial_objects import *
 
-st.header("2 Players Game")
+st.header("2 Player Game")
 
-# initialise vars
+# initialise blank string vars
 var_list = ['card_1_1',
+            'card_1_2',
+            'card_1_3',
+            'card_1_4',
+            'card_2_1',
+            'card_2_2',
+            'card_2_3',
+            'card_2_4',
+            'card_3_1',
+            'card_3_2',
+            'card_3_3',
+            'card_3_4',
+            'setup_complete',
+            'turn',
+            'obj_1',
+            'obj_2',
+            'obj_3',
+            'tableau_deck',
+            'p1_deck',
+            'cpu_deck',
+            'initialise'
+            ]
+ 
+for var in var_list:
+    if var not in st.session_state:
+        if var == 'turn':
+            st.session_state[var] = 1
+        elif var in ['tableau_deck', 'p1_deck', 'cpu_deck']:
+            st.session_state[var] = []
+        else:
+            st.session_state[var] = ''
+ 
+ 
+# List of cards
+if 'card_serials' not in st.session_state:
+    st.session_state['card_serials'] = ['bla1002020',
+                    'bla1002100',
+                    'bla1003000',
+                    'bla1011110',
+                    'bla1020120',
+                    'bla1021110',
+                    'bla1040001',
+                    'bla1101300',
+                    'blu1000401',
+                    'blu1002210',
+                    'blu1101110',
+                    'blu1101210',
+                    'blu1200010',
+                    'blu1202000',
+                    'blu1300000',
+                    'blu1013100',
+                    'gre1000300',
+                    'gre1010020',
+                    'gre1020200',
+                    'gre1031010',
+                    'gre1110110',
+                    'gre1210110',
+                    'gre1210200',
+                    'gre1400001',
+                    'red1000030',
+                    'red1000041',
+                    'red1000220',
+                    'red1021000',
+                    'red1111010',
+                    'red1111020',
+                    'red1201020',
+                    'red1300110',
+                    'whi1004001',
+                    'whi1030000',
+                    'whi1100200',
+                    'whi1110030',
+                    'whi1111100',
+                    'whi1112100',
+                    'whi1122000',
+                    'whi1220000']
+    
+
+if 'obj_serials' not in st.session_state:
+    st.session_state['obj_serials'] = ['obj004403',
+                   'obj033033',
+                   'obj040043',
+                   'obj044003',
+                   'obj300333',
+                   'obj303303',
+                   'obj330033',
+                   'obj400043',
+                   'obj400403'
+                   ]
+    
+# Initialise card slots
+if 'slots' not in st.session_state:
+    st.session_state['slots'] = ['card_1_1',
              'card_1_2',
              'card_1_3',
              'card_1_4',
@@ -22,135 +112,49 @@ var_list = ['card_1_1',
              'card_3_1',
              'card_3_2',
              'card_3_3',
-             'card_3_4',
-             'setup_complete',
-             'turn',
-             'obj_1',
-             'obj_2',
-             'obj_3',
-             'deck',
-             'tableau_deck',
-             'p1_deck',
-             'cpu_deck',
-             'initialise'
-             ]
+             'card_3_4']
 
-for var in var_list:
-    if var not in st.session_state:
-        if var == 'turn':
-            st.session_state[var] = 1
-        elif var in ['deck', 'tableau_deck', 'p1_deck', 'cpu_deck']:
-            st.session_state[var] = []
-        else:
-            st.session_state[var] = ''
-
-
-# List of cards
-card_serials = ['bla1002020',
-                'bla1002100',
-                'bla1003000',
-                'bla1011110',
-                'bla1020120',
-                'bla1021110',
-                'bla1040001',
-                'bla1101300',
-                'blu1000401',
-                'blu1002210',
-                'blu1101110',
-                'blu1101210',
-                'blu1200010',
-                'blu1202000',
-                'blu1300000',
-                'blu1013100',
-                'gre1000300',
-                'gre1010020',
-                'gre1020200',
-                'gre1031010',
-                'gre1110110',
-                'gre1210110',
-                'gre1210200',
-                'gre1400001',
-                'red1000030',
-                'red1000041',
-                'red1000220',
-                'red1021000',
-                'red1111010',
-                'red1111020',
-                'red1201020',
-                'red1300110',
-                'whi1004001',
-                'whi1030000',
-                'whi1100200',
-                'whi1110030',
-                'whi1111100',
-                'whi1112100',
-                'whi1122000',
-                'whi1220000']
-
-obj_serials = ['obj004403',
-               'obj033033',
-               'obj040043',
-               'obj044003',
-               'obj300333',
-               'obj303303',
-               'obj330033',
-               'obj400043',
-               'obj400403'
-               ]
-
-
-slots = ['card_1_1',
-         'card_1_2',
-         'card_1_3',
-         'card_1_4',
-         'card_2_1',
-         'card_2_2',
-         'card_2_3',
-         'card_2_4',
-         'card_3_1',
-         'card_3_2',
-         'card_3_3',
-         'card_3_4']
-
+# Initialise main deck
+if 'main_deck' not in st.session_state:
+    st.session_state['main_deck'] = []
+    for serial in st.session_state['card_serials']:
+        st.session_state['main_deck'] = st.session_state['main_deck'] + [serial]
+        
+ 
 # confirm button
 def confirm_button():
-    # create deck of cards (dictionary)
-    for serial in card_serials:
-        
-        # add to deck
-        st.session_state['deck'] = st.session_state['deck'] + [serial]
-    
-    st.session_state['setup_complete'] = 'complete'
     
     # add drawn cards to tableau
-    for card in slots:
+    for card in st.session_state['slots']:
         st.session_state['tableau_deck'] = st.session_state['tableau_deck'] + [st.session_state[card]]
         
+    st.session_state['setup_complete'] = 'complete'
+        
     # remove drawn cards from deck
-    st.session_state['deck'] = [card for card in st.session_state['deck'] if card not in st.session_state['tableau_deck']]
-    
-# intialise cards_dict
-cards_dict = {'':'cards/splendor.jpg'}
+    st.session_state['main_deck'] = [card for card in st.session_state['main_deck'] if card not in st.session_state['tableau_deck']]
+     
+# Initialise cards_img_dict
+if 'card_img_dict' not in st.session_state:
+    st.session_state['card_img_dict'] = {'':"cards/splendor.jpg"}
+    for card in st.session_state['card_serials']:
+        st.session_state['card_img_dict'][card] = "cards/" + card + ".jpg"
+        
+    for key in st.session_state['card_img_dict'].keys():
+        st.session_state['card_img_dict'][key] = ImageOps.exif_transpose(Image.open(st.session_state['card_img_dict'][key]))
 
-for card in card_serials:
-    cards_dict[card] = "cards/" + card + ".jpg"
-    
-for key in cards_dict.keys():
-    cards_dict[key] = ImageOps.exif_transpose(Image.open(cards_dict[key]))
-
-# initialise objs_dict
-objs_dict = {'':'cards/splendor.jpg'}
-
-for obj in obj_serials:
-    objs_dict[obj] = "cards/objective_cards/" + obj + ".jpg"
-
-for key in objs_dict.keys():
-    objs_dict[key] = ImageOps.exif_transpose(Image.open(objs_dict[key]))
- 
-
-st.write("Open the toggle to set up initial card tableau. Close when done")
+# Initialise obj_img_dict
+if 'obj_img_dict' not in st.session_state:
+    st.session_state['obj_img_dict'] = {'':'cards/objective_cards/splendor.jpg'}
+    for obj in st.session_state['obj_serials']:
+        st.session_state['obj_img_dict'][obj] = "cards/objective_cards/" + obj + ".jpg"
+        
+    for key in st.session_state['obj_img_dict'].keys():
+        st.session_state['obj_img_dict'][key] = ImageOps.exif_transpose(Image.open(st.session_state['obj_img_dict'][key]))
+        
+# Initial set up interface 
+st.write("Open the toggle to set up initial card tableau. Click Confirm setup when done")
 if st.toggle("Game setup") and st.session_state['setup_complete']=='': 
-    st.write("Please enter cards in colour-cost-points format:\n\n bla/blu/gre/red/whi for colour\n\n a 1 digit number for level\n\n a 5 digit number for the cost for the respective colours\n\n a 1 digit number for the points\n\n e.g. blu1333001")
+    st.write("Please enter cards in colour-level-cost-points format:\n\n bla/blu/gre/red/whi for colour\n\n a 1 digit number for level\n\n a 5 digit number for the cost for the respective colours\n\n a 1 digit number for the points\n\n e.g. blu1333001\n\n Omit level for objective cards")
     
     obj_cols = st.columns(3)
     
@@ -193,57 +197,57 @@ obj_tableau = st.columns(3)
 
 with obj_tableau[0]:
     obj_slot_1 = st.empty()
-    obj_slot_1.image(cards_dict[st.session_state['obj_1']])
+    obj_slot_1.image(st.session_state['obj_img_dict'][st.session_state['obj_1']])
     
 with obj_tableau[1]:
     obj_slot_2 = st.empty()
-    obj_slot_2.image(cards_dict[st.session_state['obj_2']])
+    obj_slot_2.image(st.session_state['obj_img_dict'][st.session_state['obj_2']])
     
 with obj_tableau[2]:
     obj_slot_3 = st.empty()
-    obj_slot_3.image(cards_dict[st.session_state['obj_3']])
+    obj_slot_3.image(st.session_state['obj_img_dict'][st.session_state['obj_3']])
     
 tableau = st.columns(4)
 
 with tableau[0]:
     slot_1_1 = st.empty()
-    slot_1_1.image(cards_dict[st.session_state['card_1_1']])
+    slot_1_1.image(st.session_state['card_img_dict'][st.session_state['card_1_1']])
     
     slot_2_1 = st.empty()
-    slot_2_1.image(cards_dict[st.session_state['card_2_1']])
+    slot_2_1.image(st.session_state['card_img_dict'][st.session_state['card_2_1']])
     
     slot_3_1 = st.empty()
-    slot_3_1.image(cards_dict[st.session_state['card_3_1']])
-    
+    slot_3_1.image(st.session_state['card_img_dict'][st.session_state['card_3_1']])
+   
 with tableau[1]:
     slot_1_2 = st.empty()
-    slot_1_2.image(cards_dict[st.session_state['card_1_2']])
+    slot_1_2.image(st.session_state['card_img_dict'][st.session_state['card_1_2']])
     
     slot_2_2 = st.empty()
-    slot_2_2.image(cards_dict[st.session_state['card_2_2']])
+    slot_2_2.image(st.session_state['card_img_dict'][st.session_state['card_2_2']])
     
     slot_3_2 = st.empty()
-    slot_3_2.image(cards_dict[st.session_state['card_3_2']])
+    slot_3_2.image(st.session_state['card_img_dict'][st.session_state['card_3_2']])
     
 with tableau[2]:
     slot_1_3 = st.empty()
-    slot_1_3.image(cards_dict[st.session_state['card_1_3']])
+    slot_1_3.image(st.session_state['card_img_dict'][st.session_state['card_1_3']])
     
     slot_2_3 = st.empty()
-    slot_2_3.image(cards_dict[st.session_state['card_2_3']])
+    slot_2_3.image(st.session_state['card_img_dict'][st.session_state['card_2_3']])
     
     slot_3_3 = st.empty()
-    slot_3_3.image(cards_dict[st.session_state['card_3_3']])
+    slot_3_3.image(st.session_state['card_img_dict'][st.session_state['card_3_3']])
     
 with tableau[3]:
     slot_1_4 = st.empty()
-    slot_1_4.image(cards_dict[st.session_state['card_1_4']])
+    slot_1_4.image(st.session_state['card_img_dict'][st.session_state['card_1_4']])
     
     slot_2_4 = st.empty()
-    slot_2_4.image(cards_dict[st.session_state['card_2_4']])
+    slot_2_4.image(st.session_state['card_img_dict'][st.session_state['card_2_4']])
     
     slot_3_4 = st.empty()
-    slot_3_4.image(cards_dict[st.session_state['card_3_4']])
+    slot_3_4.image(st.session_state['card_img_dict'][st.session_state['card_3_4']])
 
 ###############
 #def xyz():
@@ -255,13 +259,10 @@ with tableau[3]:
 ###############
 
 
-st.write("Deck contains: " + str(len((st.session_state['deck']))))
+st.write("Deck contains: " + str(len((st.session_state['main_deck']))))
 st.write("Tableau contains: " + str(len(st.session_state['tableau_deck'])))
-#st.write("sst.session_state['card_1_1'])
-st.write("decks:")
-st.write(st.session_state['deck'])
-st.write(st.session_state['tableau_deck'])
-st.session_state
+#st.write("session state:")
+#st.session_state
 
     
     
